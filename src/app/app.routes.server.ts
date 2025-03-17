deefdb026dfac562f67335d5154d624e82028446
João Paulo Dias <joaopdias.dev@gmail.com>
2025-03-17 15:42:57 -0300

    fix: adjust the render for the most pages

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,7 @@ import { RenderMode, ServerRoute } from "@angular/ssr";
 
 export const serverRoutes: ServerRoute[] = [
   { path: "", renderMode: RenderMode.Server },
-  { path: "access", renderMode: RenderMode.Server },
-  { path: "post/create", renderMode: RenderMode.Server },
+  { path: "access", renderMode: RenderMode.Prerender },
+  { path: "post/create", renderMode: RenderMode.Prerender },
   { path: "post/:id", renderMode: RenderMode.Prerender },
 ];
