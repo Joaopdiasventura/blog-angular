1eceb491b16dc34151dd19e9d511816907fcc2af
João Paulo Dias <joaopdias.dev@gmail.com>
2025-03-17 09:45:00 -0300

    fix: adjust the render for the home page

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,7 +1,7 @@
 import { RenderMode, ServerRoute } from "@angular/ssr";
 
 export const serverRoutes: ServerRoute[] = [
-  { path: "", renderMode: RenderMode.Prerender },
+  { path: "", renderMode: RenderMode.Server },
   { path: "access", renderMode: RenderMode.Server },
   { path: "post/create", renderMode: RenderMode.Server },
   { path: "post/:id", renderMode: RenderMode.Prerender },
